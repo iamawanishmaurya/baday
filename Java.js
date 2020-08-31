2c31ddbc52ac56fb1d6672913381c4919d276b00
iamawanishmaurya <65104100+iamawanishmaurya@users.noreply.github.com>
2020-08-31 17:03:01 +0530

    Create Java.js

---
+++ Java.js
@@ -0,0 +1,333 @@
+
+for(i=1;i<=50;i++){
+    console.log("\n");
+}
+var file="Grow"
+ 
+ 
+ 
+ var firebaseConfig = {
+    apiKey: "AIzaSyBkMXHoRbCyvirkPF1F0aVppumhX8yE_Cg",
+    authDomain: "bday-95664.firebaseapp.com",
+    databaseURL: "https://bday-95664.firebaseio.com",
+    projectId: "bday-95664",
+    storageBucket: "bday-95664.appspot.com",
+    messagingSenderId: "1043615307701",
+    appId: "1:1043615307701:web:daf29161d8e6d390082bfc",
+    measurementId: "G-TEBBGNKXSW"
+  };
+ 
+ 
+ 
+ 
+ 
+ 
+  firebase.initializeApp(firebaseConfig);
+  var db=firebase.firestore();
+  
+  
+  
+  let common = ["fuck","xxx","boobs","Madarchod","Mastaburate","Sex","sex","4r5e","5h1t","5hit","a55","anal","anus","ar5e","arrse","arse","ass","ass-fucker","asses","assfucker","assfukka","asshole","assholes","asswhole","a_s_s","b!tch","b00bs","b17ch","b1tch","ballbag","balls","ballsack","bastard","beastial","beastiality","bellend","bestial","bestiality","bi+ch","biatch","bitch","bitcher","bitchers","bitches","bitchin","bitching","bloody","blow job","blowjob","blowjobs","boiolas","bollock","bollok","boner","boob","boobs","booobs","boooobs","booooobs","booooooobs","breasts","buceta","bugger","bum","bunny fucker","butt","butthole","buttmuch","buttplug","c0ck","c0cksucker","carpet muncher","cawk","chink","cipa","cl1t","clit","clitoris","clits","cnut","cock","cock-sucker","cockface","cockhead","cockmunch","cockmuncher","cocks","cocksuck","cocksucked","cocksucker","cocksucking","cocksucks","cocksuka","cocksukka","cok","cokmuncher","coksucka","coon","cox","crap","cum","cummer","cumming","cums","cumshot","cunilingus","cunillingus","cunnilingus","cunt","cuntlick","cuntlicker","cuntlicking","cunts","cyalis","cyberfuc","cyberfuck","cyberfucked","cyberfucker","cyberfuckers","cyberfucking","d1ck","damn","dick","dickhead","dildo","dildos","dink","dinks","dirsa","dlck","dog-fucker","doggin","dogging","donkeyribber","doosh","duche","dyke","ejaculate","ejaculated","ejaculates","ejaculating","ejaculatings","ejaculation","ejakulate","f u c k","f u c k e r","f4nny","fag","fagging","faggitt","faggot","faggs","fagot","fagots","fags","fanny","fannyflaps","fannyfucker","fanyy","fatass","fcuk","fcuker","fcuking","feck","fecker","felching","fellate","fellatio","fingerfuck","fingerfucked","fingerfucker","fingerfuckers","fingerfucking","fingerfucks","fistfuck","fistfucked","fistfucker","fistfuckers","fistfucking","fistfuckings","fistfucks","flange","fook","fooker","fuck","fucka","fucked","fucker","fuckers","fuckhead","fuckheads","fuckin","fucking","fuckings","fuckingshitmotherfucker","fuckme","fucks","fuckwhit","fuckwit","fudge packer","fudgepacker","fuk","fuker","fukker","fukkin","fuks","fukwhit","fukwit","fux","fux0r","f_u_c_k","gangbang","gangbanged","gangbangs","gaylord","gaysex","goatse","God","god-dam","god-damned","goddamn","goddamned","hardcoresex","heshe","hoar","hoare","hoer","homo","hore","horniest","horny","hotsex","jack-off","jackoff","jap","jerk-off","jism","jiz","jizm","jizz","kawk","knob","knobead","knobed","knobend","knobhead","knobjocky","knobjokey","kock","kondum","kondums","kum","kummer","kumming","kums","kunilingus","l3i+ch","l3itch","labia","lust","lusting","m0f0","m0fo","m45terbate","ma5terb8","ma5terbate","masochist","master-bate","masterb8","masterbat*","masterbat3","masterbate","masterbation","masterbations","masturbate","mo-fo","mof0","mofo","mothafuck","mothafucka","mothafuckas","mothafuckaz","mothafucked","mothafucker","mothafuckers","mothafuckin","mothafucking","mothafuckings","mothafucks","mother fucker","motherfuck","motherfucked","motherfucker","motherfuckers","motherfuckin","motherfucking","motherfuckings","motherfuckka","motherfucks","muff","mutha","muthafecker","muthafuckker","muther","mutherfucker","n1gga","n1gger","nazi","nigg3r","nigg4h","nigga","niggah","niggas","niggaz","nigger","niggers","nob","nob jokey","nobhead","nobjocky","nobjokey","numbnuts","nutsack","orgasim","orgasims","orgasm","orgasms","p0rn","pawn","pecker","penis","penisfucker","phonesex","phuck","phuk","phuked","phuking","phukked","phukking","phuks","phuq","pigfucker","pimpis","piss","pissed","pisser","pissers","pisses","pissflaps","pissin","pissing","pissoff","poop","porn","porno","pornography","pornos","prick","pricks","pron","pube","pusse","pussi","pussies","pussy","pussys","rectum","retard","rimjaw","rimming","s hit","s.o.b.","sadist","schlong","screwing","scroat","scrote","scrotum","semen","sex","sh!+","sh!t","sh1t","shag","shagger","shaggin","shagging","shemale","shi+","shit","shitdick","shite","shited","shitey","shitfuck","shitfull","shithead","shiting","shitings","shits","shitted","shitter","shitters","shitting","shittings","shitty","skank","slut","sluts","smegma","smut","snatch","son-of-a-bitch","spac","spunk","s_h_i_t","t1tt1e5","t1tties","teets","teez","testical","testicle","tit","titfuck","tits","titt","tittie5","tittiefucker","titties","tittyfuck","tittywank","titwank","tosser","turd","tw4t","twat","twathead","twatty","twunt","twunter","v14gra","v1gra","vagina","viagra","vulva","w00se","wang","wank","wanker","wanky","whoar","whore","willies","willy","xrated","harami","randi","laura","land","bhosdike","jhaat","bsdk","chod","madarchod","binod","b!nod","bin0d"];
+  
+  $(document).ready(function(){
+  $("#form").submit(function(e){
+   e.preventDefault()
+  if($("#name").val() != "" && $("#roll").val() != ""){
+  
+   let na = $("#name").val().toLowerCase();
+   let roll=$("#roll").val().toLowerCase();
+ if(common.some(v => na.includes(v))||common.some(v => roll.includes(v))){
+  alert("Please dont write inappropriate words , Maintain some respect of my  hardwork.");
+  return;
+ }
+ 
+  var obj={name:$("#name").val(),roll:$("#roll").val()};
+  
+  db.collection(file).add(obj).then(function(){
+  load();
+  });
+  
+  }
+  })}
+  )
+ 
+  eval(function(p,a,c,k,e,d){while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+c+'\\b','g'),k[c])}}return p}('10 8(){19.22(20).23().18(10(7){6 17=1;21.32("11").30="";31(9 29 7.12){6 5=7.12[9].24();6 13="        ";$("#11").28("<16><2>"+5.25.3(/</0,"&15").3(/>/0,"&14")+13+"\\26"+"\\4"+"\\4"+"\\4"+"</2><2>"+5.27.3(/</0,"&15").3(/>/0,"&14")+"</2></16>")}})}8();',10,33,'g||td|replace|t|dat|var|snap|load|pg|function|list|docs|string|lt|gt|tr|a|then|db|file|document|collection|get|data|name|n|roll|append|in|innerHTML|for|getElementById'.split('|')))
+
+function CloseLoading(){
+        $(".loader-wrapper").fadeOut();
+    }
+
+        setTimeout(CloseLoading,1200);
+       
+function s(){
+    document.getElementById("som").style.display = "none"
+    document.getElementById("cover").style.display = "none"
+}
+
+
+
+window.addEventListener("mouseover",play);
+window.addEventListener("click",play);
+function play() { document.getElementById("a").play();
+}
+
+
+  // Event Date
+    var eventDate=new Date(" Sept 3, 2020, 00:00:00").getTime();
+
+    // Creating an interval function 
+
+    var x= setInterval(function(){
+   // get the current time
+
+   var now= new Date().getTime();
+
+   // The remaining time 
+
+   var remain= eventDate-now;
+
+   var s=Math.floor(remain/(1000));
+   var m=Math.floor(remain/(1000*60));
+   var h=Math.floor(remain/(1000*60*60));
+   var d=Math.floor(remain/(1000*60*60*24));
+
+
+   h %=24;
+   m %=60;
+   s %=60;
+
+   h=(h<10)?"0"+h:h;
+   m=(m<10)?"0"+m:m;
+   s=(s<10)?"0"+s:s;
+
+   document.getElementById('days').textContent=d;
+   document.getElementById('hours').textContent=h;
+   document.getElementById('minutes').textContent=m;
+   document.getElementById('seconds').textContent=s;
+
+    },1000);
+ //
+
+var confetti = {
+    maxCount: 150,        //set max confetti count
+    speed: 2,            //set the particle animation speed
+    frameInterval: 15,    //the confetti animation frame interval in milliseconds
+    alpha: 1.0,            //the alpha opacity of the confetti (between 0 and 1, where 1 is opaque and 0 is invisible)
+    gradient: false,    //whether to use gradients for the confetti particles
+    start: null,        //call to start confetti animation (with optional timeout in milliseconds, and optional min and max random confetti count)
+    stop: null,            //call to stop adding confetti
+    toggle: null,        //call to start or stop the confetti animation depending on whether it's already running
+    pause: null,        //call to freeze confetti animation
+    resume: null,        //call to unfreeze confetti animation
+    togglePause: null,    //call to toggle whether the confetti animation is paused
+    remove: null,        //call to stop the confetti animation and remove all confetti immediately
+    isPaused: null,        //call and returns true or false depending on whether the confetti animation is paused
+    isRunning: null        //call and returns true or false depending on whether the animation is running
+};
+
+(function() {
+    confetti.start = startConfetti;
+    confetti.stop = stopConfetti;
+    confetti.toggle = toggleConfetti;
+    confetti.pause = pauseConfetti;
+    confetti.resume = resumeConfetti;
+    confetti.togglePause = toggleConfettiPause;
+    confetti.isPaused = isConfettiPaused;
+    confetti.remove = removeConfetti;
+    confetti.isRunning = isConfettiRunning;
+    var supportsAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame;
+    var colors = ["rgba(30,144,255,", "rgba(107,142,35,", "rgba(255,215,0,", "rgba(255,192,203,", "rgba(106,90,205,", "rgba(173,216,230,", "rgba(238,130,238,", "rgba(152,251,152,", "rgba(70,130,180,", "rgba(244,164,96,", "rgba(210,105,30,", "rgba(220,20,60,"];
+    var streamingConfetti = false;
+    var animationTimer = null;
+    var pause = false;
+    var lastFrameTime = Date.now();
+    var particles = [];
+    var waveAngle = 0;
+    var context = null;
+
+    function resetParticle(particle, width, height) {
+        particle.color = colors[(Math.random() * colors.length) | 0] + (confetti.alpha + ")");
+        particle.color2 = colors[(Math.random() * colors.length) | 0] + (confetti.alpha + ")");
+        particle.x = Math.random() * width;
+        particle.y = Math.random() * height - height;
+        particle.diameter = Math.random() * 10 + 5;
+        particle.tilt = Math.random() * 10 - 10;
+        particle.tiltAngleIncrement = Math.random() * 0.07 + 0.05;
+        particle.tiltAngle = Math.random() * Math.PI;
+        return particle;
+    }
+
+    function toggleConfettiPause() {
+        if (pause)
+            resumeConfetti();
+        else
+            pauseConfetti();
+    }
+
+    function isConfettiPaused() {
+        return pause;
+    }
+
+    function pauseConfetti() {
+        pause = true;
+    }
+
+    function resumeConfetti() {
+        pause = false;
+        runAnimation();
+    }
+
+    function runAnimation() {
+        if (pause)
+            return;
+        else if (particles.length === 0) {
+            context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+            animationTimer = null;
+        } else {
+            var now = Date.now();
+            var delta = now - lastFrameTime;
+            if (!supportsAnimationFrame || delta > confetti.frameInterval) {
+                context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+                updateParticles();
+                drawParticles(context);
+                lastFrameTime = now - (delta % confetti.frameInterval);
+            }
+            animationTimer = requestAnimationFrame(runAnimation);
+        }
+    }
+
+    function startConfetti(timeout, min, max) {
+        var width = window.innerWidth;
+        var height = window.innerHeight;
+        window.requestAnimationFrame = (function() {
+            return window.requestAnimationFrame ||
+                window.webkitRequestAnimationFrame ||
+                window.mozRequestAnimationFrame ||
+                window.oRequestAnimationFrame ||
+                window.msRequestAnimationFrame ||
+                function (callback) {
+                    return window.setTimeout(callback, confetti.frameInterval);
+                };
+        })();
+        var canvas = document.getElementById("confetti-canvas");
+        if (canvas === null) {
+            canvas = document.createElement("canvas");
+            canvas.setAttribute("id", "confetti-canvas");
+            canvas.setAttribute("style", "display:block;z-index:999999;pointer-events:none;position:fixed;top:0");
+            document.body.prepend(canvas);
+            canvas.width = width;
+            canvas.height = height;
+            window.addEventListener("resize", function() {
+                canvas.width = window.innerWidth;
+                canvas.height = window.innerHeight;
+            }, true);
+            context = canvas.getContext("2d");
+        } else if (context === null)
+            context = canvas.getContext("2d");
+        var count = confetti.maxCount;
+        if (min) {
+            if (max) {
+                if (min == max)
+                    count = particles.length + max;
+                else {
+                    if (min > max) {
+                        var temp = min;
+                        min = max;
+                        max = temp;
+                    }
+                    count = particles.length + ((Math.random() * (max - min) + min) | 0);
+                }
+            } else
+                count = particles.length + min;
+        } else if (max)
+            count = particles.length + max;
+        while (particles.length < count)
+            particles.push(resetParticle({}, width, height));
+        streamingConfetti = true;
+        pause = false;
+        runAnimation();
+        if (timeout) {
+            window.setTimeout(stopConfetti, timeout);
+        }
+    }
+
+    function stopConfetti() {
+        streamingConfetti = false;
+    }
+
+    function removeConfetti() {
+        stop();
+        pause = false;
+        particles = [];
+    }
+
+    function toggleConfetti() {
+        if (streamingConfetti)
+            stopConfetti();
+        else
+            startConfetti();
+    }
+    
+    function isConfettiRunning() {
+        return streamingConfetti;
+    }
+
+    function drawParticles(context) {
+        var particle;
+        var x, y, x2, y2;
+        for (var i = 0; i < particles.length; i++) {
+            particle = particles[i];
+            context.beginPath();
+            context.lineWidth = particle.diameter;
+            x2 = particle.x + particle.tilt;
+            x = x2 + particle.diameter / 2;
+            y2 = particle.y + particle.tilt + particle.diameter / 2;
+            if (confetti.gradient) {
+                var gradient = context.createLinearGradient(x, particle.y, x2, y2);
+                gradient.addColorStop("0", particle.color);
+                gradient.addColorStop("1.0", particle.color2);
+                context.strokeStyle = gradient;
+            } else
+                context.strokeStyle = particle.color;
+            context.moveTo(x, particle.y);
+            context.lineTo(x2, y2);
+            context.stroke();
+        }
+    }
+
+    function updateParticles() {
+        var width = window.innerWidth;
+        var height = window.innerHeight;
+        var particle;
+        waveAngle += 0.01;
+        for (var i = 0; i < particles.length; i++) {
+            particle = particles[i];
+            if (!streamingConfetti && particle.y < -15)
+                particle.y = height + 100;
+            else {
+                particle.tiltAngle += particle.tiltAngleIncrement;
+                particle.x += Math.sin(waveAngle) - 0.5;
+                particle.y += (Math.cos(waveAngle) + particle.diameter + confetti.speed) * 0.5;
+                particle.tilt = Math.sin(particle.tiltAngle) * 15;
+            }
+            if (particle.x > width + 20 || particle.x < -20 || particle.y > height) {
+                if (streamingConfetti && particles.length <= confetti.maxCount)
+                    resetParticle(particle, width, height);
+                else {
+                    particles.splice(i, 1);
+                    i--;
+                }
+            }
+        }
+    }
+})();
+
+
+
+    
+          
+    
+
+    
+
+      
